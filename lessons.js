06a9a5a052c73c637f2552f6cd755e7b6f96cb15
user <fiveminut@mail.ru>
2023-06-07 14:02:39 +0600

    2 task

--- lessons.js
+++ lessons.js
@@ -1,20 +1,32 @@
+// ---------------------------------------------------------------------------------
+// 2
 function spinWords(string) {
-    let word;
-    let word2;
-    for (let i = 0; string.length > i; i++) {
-        if (string[i] !== ' ') {
-            word += string[string.length-i];
-            if (word.length === string.length) break;
-        } else {
-            if (word.length>4) {
-                word2 += word;
-                if (word2.length === string.length) {
-                    break;
-                } else {
-                    word2 += ' ';
-                }
-            }
+ let arr = string.split(" ");
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i].length >= 5) {
+            arr[i] = arr[i].split("").reverse().join("");
         }
     }
+
+    let result = arr.join(" ");
+    return result;
 }
-console.log(spinWords('has already been declared\n'))
+
+function spinWords(string) {
+    return string
+        .split(" ")
+        .map(word => word.length >= 5 ? word.split("").reverse().join("") : word)
+        .join(" ");
+}
+
+console.log(spinWords("Welcome"));
+console.log(spinWords("Hey fellow warriors"));
+console.log(spinWords("This is a test"));
+console.log(spinWords("This is another test"));
+console.log(spinWords("You are almost to the last test"));
+console.log(spinWords("Just kidding there is still one more"));
+console.log(spinWords("Seriously this is the last one"));
+
+// ---------------------------------------------------------------------------------
+// 3
